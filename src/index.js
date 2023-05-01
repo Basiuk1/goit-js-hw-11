fc10ff8f330a9cb0642db3e73d53be6aa542592b
Yura Basiuk <yurik.kamenev@gmail.com>
2023-05-01 18:32:49 +0300

    finished work

--- src/index.js
+++ src/index.js
@@ -1,61 +1,70 @@
-// import simpleLightbox from 'simplelightbox';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-import axios from 'axios';
+import { createMarkup } from './js/markup';
+import { getImages } from './js/api';
 
-const gallery = document.querySelector('gallery');
+const searchForm = document.querySelector('#search-form');
+const buttonLoadMore = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
 
-async function getImages(page = 1) {
-  return await axios.get(
-    `https://pixabay.com/api?key=35817909-7eb30b2bd2710e4d1f9e56635&q&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-  );
-  // if (!resp.ok) {
-  //   throw new Error(
-  //     Notiflix.Notify(
-  //       `Sorry, there are no images matching your search query. Please try again.`
-  //     )
-  //   );
-  // }
-  // return await resp.json();
+buttonLoadMore.classList.add('is-hidden');
+
+searchForm.addEventListener('submit', onFormSubmit);
+buttonLoadMore.addEventListener('click', onBtnLoad);
+
+let lightbox = new simpleLightbox('.gallery a');
+let query = '';
+let page = 1;
+const perPage = 40;
+
+async function onFormSubmit(e) {
+  e.preventDefault();
+  page = 1;
+  gallery.innerHTML = '';
+  query = e.currentTarget.searchQuery.value.trim();
+  if (query === '') {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.'
+    );
+    return;
+  }
+  try {
+    const { data } = await getImages(query, page, perPage);
+
+    if (data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      createMarkup(data.hits);
+      lightbox.refresh();
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+      if (data.totalHits > perPage) {
+        buttonLoadMore.classList.remove('is-hidden');
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+async function onBtnLoad() {
+  page += 1;
+  try {
+    const { data } = await getImages(query, page, perPage);
 
-// getImages()
-//   .then(data => {
-//     gallery.insertAdjacentHTML('beforeend', createMarkup);
-//   })
-//   .catch(err => console.log(err));
-getImages()
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+    createMarkup(data.hits);
+    lightbox.refresh();
+    const totalPages = Math.ceil(data.totalHits / perPage);
 
-function createMarkup(images) {
-  return images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<div class="photo-card">
-      <a class="gallery__link" href="${largeImageURL}" target="_parent">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>${views}</b>
-    </p>
-    <p class="info-item">
-      <b>${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>${downloads}</b>
-    </p>
-  </div>
-</div>`
-    )
-    .join('');
+    if (page > totalPages) {
+      buttonLoadMore.classList.add('is-hidden');
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
---
+++ src/js/markup.js
@@ -0,0 +1,47 @@
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(images) {
+  const markup = images
+    .map(image => {
+      const {
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+
+      return `<a class="gallery-item" href="${largeImageURL}" >
+  <div class="gallery-item-img">
+    <img
+      class="gallery-img"
+      src="${webformatURL}"
+      alt="${tags}"
+      loading="lazy"
+    />
+    <div class="info">
+      <p class="info-item">
+        <span>Likes:</span>${likes}
+      </p>
+      <p class="info-item">
+        <span>Views:</span>${views}
+      </p>
+      <p class="info-item">
+        <span>Comments:</span>${comments}
+      </p>
+      <p class="info-item">
+        <span>Downloads:</span>${downloads}
+      </p>
+    </div>
+  </div>
+</a>`;
+    })
+    .join('');
+
+  console.log(markup);
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+export { createMarkup };
